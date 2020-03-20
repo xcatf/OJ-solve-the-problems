d0d9b7f4d564670cdad717d1cae64f82723426e4
1013459920@qq.com <1013459920@qq.com>
2020-03-20 08:06:57 +0800

    #

---
+++ LeetCode/寻找两个数组的中位数.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findKthNum(vector<int>& nums1, vector<int>& nums2, int k){
+        int left=max(0,k-(int)nums2.size());
+        int right=min(k,(int)nums1.size());
+        printf("k:%d\n", k);       
+        while(left<right){
+            int mid=left+right>>1;
+            printf("left:%d mid:%d right:%d num2[k-mid-1]:%d num1[mid]:%d\n", left,mid,right,nums2[k-mid-1],nums1[mid]);
+            if(nums2[k-mid-1]>nums1[mid]) left=mid+1;
+            else right=mid;
+        }
+        printf("left-1:%d k-left-1:%d\n",left-1,k-left-1);
+        int nums1Max = left==0?INT_MIN:nums1[left-1];
+        int nums2Max = left==k?INT_MIN:nums2[k-left-1];
+        return max(nums1Max,nums2Max);
+    }
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size()+nums2.size();
+        if(n&1){
+            return findKthNum(nums1,nums2,(n>>1)+1);
+        }
+        else{
+            return (findKthNum(nums1,nums2,n>>1) + findKthNum(nums1,nums2,(n>>1)+1))/2.0;
+        }
+    }
+};
+int main(){
+    int a[] = {1,3,5,7,9};
+    int b[] = {2,4,6,8,10};
+    vector<int>num1(a,a+5),num2(b,b+5);
+    for(int i=0;i<5;++i) printf("%d%c",a[i],i==4?'\n':' ');
+    for(int i=0;i<5;++i) printf("%d%c",b[i],i==4?'\n':' ');
+    Solution sol;
+    printf("%.2f\n", sol.findMedianSortedArrays(num1,num2));
+
+}
